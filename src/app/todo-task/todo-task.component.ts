0c5709db3b8693696142132c092029d61c23b6be
DimaMakovetsky <mister.chuppa@mail.ru>
2022-11-24 18:09:40 +0300

    lesson

---
+++ src/app/todo-task/todo-task.component.ts
@@ -0,0 +1,22 @@
+import { Task } from './../types/task.type';
+import { Component,Input,Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-task',
+  templateUrl: './todo-task.component.html',
+  styleUrls: ['./todo-task.component.scss']
+})
+export class TodoTaskComponent {
+  @Input() task: Task
+
+  @Output() remove:EventEmitter<number>=new EventEmitter<number>();
+
+  removeTask():void
+  {
+    this.remove.emit(this.task.id);
+  }
+  complete(checked:boolean):void
+  {
+    this.task.completed=checked;
+  }
+}
